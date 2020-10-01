ceaa1ca86b36b837cf79cdabffe34f0e6a733aa8
quernest <a.zhit4enko@gmail.com>
2020-10-01 20:32:09 +0300

    upd theme, add constants

---
+++ src/scripts/constants.js
@@ -0,0 +1,6 @@
+export const THEME_LOCAL_STORAGE_KEY = 'theme';
+export const THEME_DARK_CLASS = 'theme-dark';
+export const THEME_LIGHT_CLASS = 'theme-light';
+export const THEME_META_NAME = 'theme-color';
+export const SCHEME_DARK = 'dark';
+export const SCHEME_LIGHT = 'light';
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,8 +1,18 @@
-import initPrefersColorScheme from 'css-prefers-color-scheme';
+import initPrefersColorScheme from "css-prefers-color-scheme";
+
+import {
+  SCHEME_DARK,
+  SCHEME_LIGHT,
+  THEME_META_NAME,
+  THEME_DARK_CLASS,
+  THEME_LIGHT_CLASS,
+  THEME_LOCAL_STORAGE_KEY,
+} from "./constants";
 
 const prefersColorScheme = initPrefersColorScheme();
-const darkModeToggler = document.querySelector('.theme-toggler');
-const meta = document.createElement('meta');
+const currentScheme = localStorage.getItem(THEME_LOCAL_STORAGE_KEY);
+const themeToggler = document.querySelector(".theme-toggler");
+const meta = document.createElement("meta");
 const body = document.body;
 
 function getThemeColor() {
@@ -10,50 +20,48 @@ function getThemeColor() {
 }
 
 function applyDarkTheme() {
-  if (body.classList.contains('theme-light')) {
-    body.classList.remove('theme-light');
+  if (body.classList.contains(THEME_LIGHT_CLASS)) {
+    body.classList.remove(THEME_LIGHT_CLASS);
   }
 
-  body.classList.add('theme-dark');
+  body.classList.add(THEME_DARK_CLASS);
 }
 
 function applyLightTheme() {
-  if (body.classList.contains('theme-dark')) {
-    body.classList.remove('theme-dark');
+  if (body.classList.contains(THEME_DARK_CLASS)) {
+    body.classList.remove(THEME_DARK_CLASS);
   }
 
-  body.classList.add('theme-light');
+  body.classList.add(THEME_LIGHT_CLASS);
 }
 
-let chosenTheme = localStorage.getItem('theme');
-
-if (chosenTheme) {
-  prefersColorScheme.scheme = chosenTheme;
-}
-
-prefersColorScheme.scheme === 'dark' ? applyDarkTheme() : applyLightTheme();
-darkModeToggler.dataset.mode = prefersColorScheme.scheme;
-meta.name = 'theme-color';
-meta.content = getThemeColor();
-
-function onTogglerClick() {
-  if (prefersColorScheme.scheme === 'dark') {
-    prefersColorScheme.scheme = 'light';
+function onTogglerClick(event) {
+  if (prefersColorScheme.scheme === SCHEME_DARK) {
+    prefersColorScheme.scheme = SCHEME_LIGHT;
     applyLightTheme();
-  } else if (prefersColorScheme.scheme === 'light') {
-    prefersColorScheme.scheme = 'dark';
+  } else if (prefersColorScheme.scheme === SCHEME_LIGHT) {
+    prefersColorScheme.scheme = SCHEME_DARK;
     applyDarkTheme();
   } else {
     applyLightTheme();
   }
 
+  event.target.dataset.mode = prefersColorScheme.scheme;
+
   if (meta) {
     meta.content = getThemeColor();
   }
 
-  localStorage.setItem('theme', prefersColorScheme.scheme);
-  darkModeToggler.dataset.mode = prefersColorScheme.scheme;
+  localStorage.setItem(THEME_LOCAL_STORAGE_KEY, prefersColorScheme.scheme);
 }
 
-darkModeToggler.addEventListener('click', onTogglerClick);
-document.getElementsByTagName('head')[0].appendChild(meta);
+if (currentScheme) {
+  prefersColorScheme.scheme = currentScheme;
+}
+
+prefersColorScheme.scheme === "dark" ? applyDarkTheme() : applyLightTheme();
+themeToggler.dataset.mode = prefersColorScheme.scheme;
+meta.name = THEME_META_NAME;
+meta.content = getThemeColor();
+themeToggler.addEventListener("click", onTogglerClick);
+document.getElementsByTagName("head")[0].appendChild(meta);
